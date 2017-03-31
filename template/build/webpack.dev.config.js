e684e2f954221189365b443c7ab2274408798998
louis wen <75168611@qq.com>
2017-03-31 09:23:50 +0800

    Update webpack.dev.config.js

--- template/build/webpack.dev.config.js
+++ template/build/webpack.dev.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')//引用webpack模块
 //引入通用的配置文件
 var webpackBaseConfig = require("./webpack.base.config.js");
 //额外创建dev的独立实例
-var webpackDevConfig = Object.create(webpackBaseConfig);
+var webpackDevConfig = new webpackBaseConfig();
 module.exports = webpackDevConfig;
 //添加mock测试数据的拦截器模块
 module.exports.entry.vendors.push('../test/mockdata/MockMain.js');
