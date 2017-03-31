bcf10e7fdaaa226719d7fb149e7d1a3384f58b83
louis wen <75168611@qq.com>
2017-03-31 09:22:28 +0800

    Update webpack.base.config.js

--- template/build/webpack.base.config.js
+++ template/build/webpack.base.config.js
@@ -8,65 +8,67 @@ var config = require('./config.js');//得到配置文件
 var path = require('path');//引用path模块
 var webpack = require('webpack')//引用webpack模块
 
-module.exports = {
-    entry: {
-        build:['../src/main.js']//入口文件
-        ,vendors: ['jquery']
-    },
-    output: {
-        path: config.webpack.outputPath,
-        publicPath: config.webpack.publicPath,
-        filename: '[name].js'
-    },
-    module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            options: {
-                loaders: {
-                    'scss': 'vue-style-loader!css-loader!sass-loader',
-                    'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+module.exports = function(){
+    return {
+        entry: {
+            build:['../src/main.js']//入口文件
+            ,vendors: ['jquery']
+        },
+        output: {
+            path: config.webpack.outputPath,
+            publicPath: config.webpack.publicPath,
+            filename: '[name].js'
+        },
+        module: {
+            rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
                 }
+            },{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },{
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }]
+        },
+        resolve: {
+            modules: [
+                "node_modules",
+                path.resolve(__dirname, "../src/lib")
+            ],
+            alias: {
+                'vue$': 'vue/dist/vue.common.js'
+                ,'jquery': 'jquery/jquery-1.11.3.js'
             }
-        },{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        },{
-            test: /\.(png|jpg|gif|svg)$/,
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]?[hash]'
-            }
-        },{
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
-        }]
-    },
-    resolve: {
-        modules: [
-            "node_modules",
-            path.resolve(__dirname, "../src/lib")
-        ],
-        alias: {
-            'vue$': 'vue/dist/vue.common.js'
-            ,'jquery': 'jquery/jquery-1.11.3.js'
-        }
-    },
-    devServer: {
-        historyApiFallback: true,
-        noInfo: true
-    },
-    performance: {
-        hints: false
-    },
-    devtool: '#eval-source-map',
-    plugins:[
-        new webpack.HotModuleReplacementPlugin(),
-        // //第三方插件 分离的插件
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendors",
-            filename: "vendors.js"
-        })
-    ]
-}
+        },
+        devServer: {
+            historyApiFallback: true,
+            noInfo: true
+        },
+        performance: {
+            hints: false
+        },
+        devtool: '#eval-source-map',
+        plugins:[
+            new webpack.HotModuleReplacementPlugin(),
+            // //第三方插件 分离的插件
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "vendors",
+                filename: "vendors.js"
+            })
+        ]
+    };
+};
